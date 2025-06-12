6fe57268d125978c51806596bd2a183c4d4c3b6e
Antonio <antonioserramitjana2005@gmail.com>
2025-06-12 09:54:39 +0200

    Configuracion de stripe del front

--- src/ventanas/detalles-cita.jsx
+++ src/ventanas/detalles-cita.jsx
@@ -2,9 +2,35 @@ import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { jwtDecode } from "jwt-decode"
 import { useNavigate } from "react-router-dom"
+import { loadStripe } from '@stripe/stripe-js';
 
 
+//VENTANA DE LOS DETALLES DE CADA CITA
 export default function DetallesCita(){
+    //clave privada para cargar Stripe
+    const stripePromise = loadStripe("pk_test_51RTRRZQiG2patSJqmXbdVTxUDhdk0LLlVEejFohG3neqgVvwIUiRGySGBuYBwIErZOgmAP0NvkMsGbiW1CyLcHPc008kydjnjc");
+
+    const token = localStorage.getItem("token")
+
+    async function handlePago(cita) {
+        const res = await fetch("http://localhost:8080/api/pagos/crear-sesion", {
+            method: "POST",
+            headers: { 
+                "Content-Type": "application/json",
+              'Authorization': `Bearer ${token}` 
+            
+            },
+            body: JSON.stringify({
+                idCita: cita.id,
+                precio: 2000 // SIEMPRE SON 20 EUROS
+            })
+        });
+
+        const data = await res.json();
+        const stripe = await stripePromise;
+        stripe.redirectToCheckout({ sessionId: data.id });
+    }
+
     const [usuarioActual, setUsuarioActual] = useState(null)
 
     const [cita, setCita] = useState(null)
@@ -20,11 +46,18 @@ export default function DetallesCita(){
     if(token){
         const user = jwtDecode(token)
         setUsuarioActual(user)
+    }else{
+        navigate("/")
     }
     
     const fetchCita = async () => {
         try {
-        const res = await fetch(`http://localhost:8080/api/citas/${id}`);
+        const res = await fetch(`http://localhost:8080/api/citas/${id}`,{
+            headers: { 
+              "Content-Type": "application/json",
+              'Authorization': `Bearer ${token}` 
+            }
+        });
         const data = await res.json()
         setCita(data)
         } catch (error) {
@@ -36,12 +69,58 @@ export default function DetallesCita(){
     
     }, [])
 
+    useEffect(() => {
+        if(usuarioActual && cita && !usuarioActual.esTatuador && usuarioActual.uid !== cita.usuario.uid){
+            navigate("/unauthorized")
+        }
+    }, [cita])
+
+    const handleBorrar = async (e) => {
+        e.preventDefault()
+        fetch(`http://localhost:8080/api/citas/borrar/${cita.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}` 
+            }
+        }).then(res => {
+            if (res.ok) {
+                alert('Cita borrada correctamente.');
+                navigate("/gestionar-citas")
+            } else {
+                alert('Error al completar la cita.');
+            }
+        })
+    }
+
+    const handleCompletar = async (e) => {
+        e.preventDefault()
+        fetch("http://localhost:8080/api/citas/completar", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}` 
+            },
+            body: JSON.stringify({
+                idCita: cita.id
+            })
+        }).then(res => {
+            if (res.ok) {
+                alert('Cita completada correctamente.');
+                navigate("/gestionar-citas")
+            } else {
+                alert('Error al completar la cita.');
+            }
+        })
+    }
+
     const handleAceptar = async (e) => {
-        e.preventDefault
+        e.preventDefault()
         fetch("http://localhost:8080/api/citas/aceptar", {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}` 
             },
             body: JSON.stringify({
                 idCita: cita.id
@@ -49,6 +128,7 @@ export default function DetallesCita(){
         }).then(res => {
             if (res.ok) {
                 alert('Cita aceptada correctamente.');
+                navigate("/gestionar-citas")
             } else {
                 alert('Error al rechazar la cita.');
             }
@@ -62,7 +142,8 @@ export default function DetallesCita(){
         fetch('http://localhost:8080/api/citas/rechazar', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}` 
             },
             body: JSON.stringify({
                 idCita: cita.id,
@@ -83,10 +164,8 @@ export default function DetallesCita(){
     <div className="appointment-container">
         {cita && (
         <>
-        
-            <form onSubmit={handleAceptar} className="appointment-form">
-                {/* Formulario original intacto */}
 
+                {/* MUESTRA LOS DATOS DEL USUARIO */}
                 <h1 className="appointment-title">Datos del usuario</h1>
                 <div className="appointment-grid">
                     <div className="appointment-form-group">
@@ -121,6 +200,7 @@ export default function DetallesCita(){
                     </div>
                 </div> 
 
+                {/* MOSTRAR DATOS DEL TATUAJE */}
                 <h1 className="appointment-title">Datos del tatuaje</h1>
                 <div className="flex flex-col items-center gap-2">
                     <label htmlFor="image" className="appointment-label">Imagen del diseño</label>
@@ -132,6 +212,7 @@ export default function DetallesCita(){
                     </div>
                 </div>
 
+                {/* DATOS DE LA CITA */}
                 <div className="appointment-grid">
                     <div className="appointment-form-group">
                         <label htmlFor="date" className="appointment-label">Fecha deseada</label>
@@ -159,7 +240,6 @@ export default function DetallesCita(){
 
                 <div className="appointment-grid">
                     <div className="appointment-form-group">
-                        {/* Input del tatuador que realizará el tatuaje solo lectura, es el mismo que hace diseño */}
                         <label htmlFor="artist" className="appointment-label">Tatuador <span className="text-xs text-gray-500 ml-2">(Los diseños los hace el responsable del mismo)</span></label>
                         <input type="text" id="artista" className="appointment-input" defaultValue={cita.tatuador.nombre || ""} readOnly="readonly"/>
                     </div>
@@ -175,29 +255,80 @@ export default function DetallesCita(){
                         readOnly
                     />
                 </div>
-                
-                <div type="submit" className="appointment-form-group">
-                    <button className="upload-submit-button">
-                        Aceptar
-                    </button>   
-                </div>
 
-            </form>
-                    <br></br>
-            <form onSubmit={handleRechazar}>
-                <div className="appointment-form-group">
-                    <label htmlFor="motivoRechazo" className="appointment-label">Motivos rechazo del tatuaje</label>
-                    <textarea
-                        id="motivoRechazo"
-                        placeholder="Especifica el motivo del rechazo de la solicitud para cita del tatuaje"
-                        className="appointment-textarea"
-                        required
-                    />
-                    <button className="upload-submit-button">
-                        Rechazar
-                    </button>
-                </div>
-            </form>
+                {/* SI LA CITA ESTA EN ESTADO PENDIENTE, APARECERA AL TATUADOR UN BOTON PARA ACEPTAR Y OTRO PARA RECHAZAR */}
+            {usuarioActual.esTatuador && cita.estado === 'pendiente' && (
+            <>
+                <form onSubmit={handleAceptar} className="appointment-form">
+                    <div type="submit" className="appointment-form-group">
+                        <button className="upload-submit-button">
+                            Aceptar
+                        </button>   
+                    </div>
+
+                </form>
+                        <br></br>
+                <form onSubmit={handleRechazar}>
+                    <div className="appointment-form-group">
+                        <label htmlFor="motivoRechazo" className="appointment-label">Motivos rechazo del tatuaje</label>
+                        <textarea
+                            id="motivoRechazo"
+                            placeholder="Especifica el motivo del rechazo de la solicitud para cita del tatuaje"
+                            className="appointment-textarea"
+                            required
+                        />
+                        <button className="upload-submit-button">
+                            Rechazar
+                        </button>
+                    </div>
+                </form>
+            </>
+            )
+            }
+
+            {/* EN CASO DE ESTAR ACEPTADA EL USUARIO LE APARECERA UNO PARA PAGARLA */}
+            {usuarioActual.uid === cita.usuario.uid && cita.estado === 'Aceptada' && (
+            <>
+                <form onSubmit={(e) => { e.preventDefault(); handlePago(cita); }}>
+                    <div className="appointment-form-group">
+                        <button type="submit" className="upload-submit-button">
+                            Pagar reserva - 20€
+                        </button>
+                    </div>
+                </form>
+            </>
+            )
+            }
+
+            {/* UNA VEZ PAGADA EL TATUADOR TENDRA UN BOTON PARA COMPLETAR DICHA CITA */}
+            {usuarioActual.esTatuador && cita.estado === 'Confirmada' && (
+            <>
+                <form onSubmit={handleCompletar} className="appointment-form">
+                    <div type="submit" className="appointment-form-group">
+                        <button className="upload-submit-button">
+                            Completar cita
+                        </button>   
+                    </div>
+
+                </form>
+            </>
+            )
+            }
+
+            {/* SI EL ESTADO NO ES PENDIENTE APARECERA AL TATUADOR LA OPCION DE PODER BORRAR LA CITA */}
+            {usuarioActual.esTatuador && cita.estado !== 'pendiente' && (
+            <>
+                <form onSubmit={handleBorrar} className="appointment-form">
+                    <div type="submit" className="appointment-form-group">
+                        <button className="upload-submit-button">
+                            Borrar cita
+                        </button>   
+                    </div>
+
+                </form>
+            </>
+            )
+            }
                     
                 
 
